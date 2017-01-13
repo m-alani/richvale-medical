d314b3d4989c5f724d5028d56b06b70bec1cc057
Marwan Alani <marwan@alani.ca>
2017-01-12 21:06:40 -0500

    Added another slide

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
   $('.slider').slider(
     {
       full_width: true,
-      interval: 4000,
+      interval: 3000,
       indicators: false,
       height: (window.screen.width < 601 ? 400 : 600)
     }
   );
 
   setTimeout(function () {
-    $('.navbar-fixed').animate({ marginTop: '0px' }, 1000);
-  }, 3000);
+    $('.navbar-fixed').animate({ marginTop: '0px' }, 500);
+  }, 2500);
 })
