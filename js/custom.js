9d08577602f75dbd956aee195820d7d11f57ede1
Marwan Alani <marwan@alani.ca>
2017-01-14 17:07:21 -0500

    Added background image. Minified files

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+    $("#facebook-link").attr("href", "fb://profile/233576250401566");
+  }
+
+  $('.slider').slider(
+    {
+      full_width: true,
+      interval: 3000,
+      indicators: true,
+      height: (window.screen.width < 601 ? 400 : 600)
+    }
+  );
+
+  setTimeout(function () {
+    $('.navbar-fixed').animate({ marginTop: '0px' }, 500);
+  }, 2500);
+})
