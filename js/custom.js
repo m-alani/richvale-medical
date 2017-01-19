bf7ce9ae91a7e1c285a9c15c18effc5d2fa52e65
Marwan Alani <marwan@alani.ca>
2017-01-19 02:47:50 -0500

    Working on the nav menu for mobile

--- js/custom.js
+++ js/custom.js
@@ -15,9 +15,37 @@ $(document).ready(function () {
 
   if (window.screen.width > 600) {
     $("#brand-text").html(' Richvale Medical Center <span class="small" >&amp; Walk-in Clinic</span>');
+  } else {
+    $("#brand-text").html(' Richvale Medical');
   }
+
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 10, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
 
   setTimeout(function () {
     $('.navbar-fixed').animate({ marginTop: '0px' }, 250);
   }, 3500);
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        alert(target.offset().top);
+        var scrollTo = target.offset().top ? target.offset().top - 62 : 0;
+        $('html, body').animate({
+          scrollTop: scrollTo
+        }, 1000);
+        return false;
+      }
+    }
+  });
 })
